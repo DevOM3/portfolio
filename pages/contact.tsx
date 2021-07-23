5b46cc44b741f42d2f375ec5993cb2763f14ab26
DevOM <oplondhe@gmail.com>
2021-07-23 23:40:00 +0530

    After creating Contact page skelaton

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import contactStyles from "../styles/pages/Contact.module.css";
 
 const contact = () => {
-  return <div></div>;
+  const onContactInformationSubmit = () => {};
+
+  return (
+    <div className={contactStyles.contact}>
+      <form
+        onSubmit={onContactInformationSubmit}
+        className={contactStyles.form}
+      >
+        <div className={contactStyles.inputContainer}>
+          <label htmlFor="name">Name</label>
+          <input id="name" className={contactStyles.input} type="text" />
+        </div>
+        <div className={contactStyles.inputContainer}>
+          <label htmlFor="email">
+            Email <small>(optional)</small>
+          </label>
+          <input id="email" className={contactStyles.input} type="email" />
+        </div>
+        <div className={contactStyles.inputContainer}>
+          <label htmlFor="description">Description</label>
+          <textarea id="description" className={contactStyles.input} />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default contact;
