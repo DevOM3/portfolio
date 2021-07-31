fd68571a9175acb640e9d639ab0ad7d46e7e8f92
DevOM <oplondhe@gmail.com>
2021-07-31 20:18:09 +0530

    After creating Admin folder and its index file

--- components/commons/Layout.tsx
+++ components/commons/Layout.tsx
@@ -1,9 +1,11 @@
+import { useRouter } from "next/dist/client/router";
 import React, { useEffect, useState } from "react";
 import layoutStyles from "../../styles/components/commons/Layout.module.css";
 import AppBar from "./AppBar";
 import Navbar from "./Navbar";
 
 const Layout = ({ children }: any) => {
+  const router = useRouter();
   const [open, setOpen] = useState(true);
 
   const updateWindowWidth = (mediaQuery: MediaQueryList) => {
@@ -22,9 +24,9 @@ const Layout = ({ children }: any) => {
 
   return (
     <div className={layoutStyles.layout}>
-      <AppBar open={open} setOpen={setOpen} />
+      {router.pathname !== "/admin" && <AppBar open={open} setOpen={setOpen} />}
       <div className={layoutStyles.navigationContainer}>
-        <Navbar open={open} />
+        {router.pathname !== "/admin" && <Navbar open={open} />}
         <div className={layoutStyles.main}>{children}</div>
       </div>
     </div>
---
+++ pages/admin/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import adminStyles from "../../styles/pages/admin/Admin.module.css";
+
+const Admin = () => {
+  return (
+    <div className={adminStyles.admin}>
+      <p>Admin panel</p>
+    </div>
+  );
+};
+
+export default Admin;
